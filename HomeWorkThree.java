ba37a9651fd9e57ee591ee0879d5955c4b599af9
Bulat Blizniuk <bulat.bliznyuk@gmail.com>
2021-09-27 06:20:21 +0500

    Java Course Level One Lesson 3 Homework Bulat Bliznyuk

---
+++ HomeWorkThree.java
@@ -0,0 +1,124 @@
+package ru.gb;
+
+public class HomeWorkThree {
+
+    public static void  main (String args[]) {
+
+        /*
+        1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+        Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+         */
+        System.out.println("Задача 1.");
+        int[] arrayOne = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println("Первоначальный массив:");
+        for (int j = 0; j < arrayOne.length; j++) {
+            System.out.print(arrayOne[j] + " ");
+        }
+        System.out.println("\nИнвертированный массив");
+        for(int i = 0; i < arrayOne.length; i++) {
+
+            if(arrayOne[i] == 0) {
+                arrayOne[i] = 1;
+            } else {
+                arrayOne[i] = 0;
+            }
+            System.out.print(arrayOne[i] + " ");
+        }
+
+        /*
+        2. Задать пустой целочисленный массив длиной 100.
+        С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
+         */
+        System.out.println("\nЗадача 2.");
+        int[] hundred = new int[100];
+        for(int i = 0; i < 100; i++) {
+            hundred[i] = i+1;
+            System.out.print(hundred[i]+" ");
+        }
+
+        /*
+        3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ]
+        пройти по нему циклом, и числа меньшие 6 умножить на 2;
+         */
+        System.out.println("\nЗадача 3.");
+        int[] toDouble = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("Первоначальный массив:");
+        for(int i = 0; i < toDouble.length; i++) {
+            System.out.print(toDouble[i] + " ");
+        }
+        System.out.println("\nИзмененный массив:");
+        for (int j = 0; j < toDouble.length; j++) {
+            if (toDouble[j] < 6) {
+                toDouble[j] = toDouble[j] * 2;
+            }
+            System.out.print(toDouble[j] + " ");
+        }
+
+        /*
+        4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+        и с помощью цикла(-ов) заполнить его диагональные элементы единицами
+        (можно только одну из диагоналей, если обе сложно). Определить элементы одной из диагоналей можно
+        по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
+         */
+        System.out.println("\nЗадача 4.");
+        int[][] matrix = new int[5][5];
+        for(int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == j) {
+                    matrix[i][j] = 1;
+                }
+                System.out.print(matrix[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+        // Вызов задачи 5:
+        System.out.println("Задача 5:");
+        createArray(5, 0);
+
+        /*
+        6. * Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
+         */
+
+        int[] minMax = {2, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 2};
+        System.out.println("\nЗадача 6:");
+        System.out.println("В массиве");
+        int min = minMax[0];
+        int max = minMax[0];
+        for (int i = 0; i < minMax.length; i++) {
+            System.out.print(minMax[i] + " ");
+            if(min > minMax[i]) {
+                min = minMax[i];
+            }
+            if (max <= minMax[i]) {
+                max = minMax[i];
+            }
+        }
+        System.out.println("\nминимальный элемент равен " + min + ", а максимальный равен " + max);
+
+    }
+
+    /*
+    5. Написать метод, принимающий на вход два аргумента: len и initialValue,
+     и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue;
+     */
+    public static int[] createArray(int len, int initialValue) {
+        int[] array = new int[len];
+        System.out.println("Новый массив размером " + len + " и начальным значением " + initialValue + " :");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = initialValue;
+            System.out.print(array[i] + " ");
+        }
+        return array;
+    }
+
+    public static boolean checkBalance(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+
+        }
+
+        return false;
+    }
+
+}

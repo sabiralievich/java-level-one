f59d9edb777175a9656a0371e26e21b2b6d0338f
Bulat Blizniuk <bulat.bliznyuk@gmail.com>
2021-10-23 20:51:22 +0500

    Video Game Catch the Drop Bulat Bliznyuk

---
+++ GameWindow.java
@@ -0,0 +1,75 @@
+package ru.gb;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+
+public class GameWindow extends JFrame {
+
+    private static GameWindow gameWindow = new GameWindow();
+    private static long last_frame_time;
+    private static Image background;
+    private static Image drop;
+    private static Image game_over;
+    private static float drop_left = 200;
+    private static float drop_top = -100;
+    private static float drop_v = 200;
+    private static int score;
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png"));
+        drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
+        game_over = ImageIO.read(GameWindow.class.getResourceAsStream("game_over.png"));
+        last_frame_time = System.nanoTime();
+        gameWindow = new GameWindow();
+        gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        gameWindow.setLocation(100, 100);
+        gameWindow.setSize(906, 478);
+        gameWindow.setResizable(false);
+        GameField gameField = new GameField();
+        gameField.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                float drop_right = drop_left + drop.getWidth(null);
+                float drop_bottom = drop_top + drop.getHeight(null);
+                boolean is_drop = x >= drop_left && x <= drop_right && y >= drop_top && y <= drop_bottom;
+                if(is_drop) {
+                    drop_top = -100;
+                    drop_left = (int) (Math.random() * (gameField.getWidth() - drop.getWidth(null)));
+                    drop_v = drop_v + 20;
+                    score++;
+                    gameWindow.setTitle("Score: " + score);
+                }
+            }
+        });
+        gameWindow.add(gameField);
+        gameWindow.setVisible(true);
+    }
+
+    public static void onRepaint (Graphics g) {
+        long current_time = System.nanoTime();
+        float delta_time = (current_time - last_frame_time) * 0.000000001f;
+        last_frame_time = current_time;
+        drop_top = drop_top + drop_v * delta_time;
+        //    drop_left = drop_left + drop_v * delta_time * 0.2f;
+        g.drawImage(background, 0,0, null);
+        g.drawImage(drop, (int) drop_left,(int) drop_top,null);
+        if(drop_top > gameWindow.getHeight()) g.drawImage(game_over,280, 120, null);
+    }
+
+    public static class GameField extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            onRepaint(g);
+            repaint();
+        }
+    }
+
+}
+

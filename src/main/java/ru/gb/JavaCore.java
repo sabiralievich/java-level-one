8430216a07eb2c62ba11a62a6bfbdcdaf49080e6
Bulat Blizniuk <bulat.bliznyuk@gmail.com>
2021-11-23 16:29:16 +0500

    hw-9

---
+++ src/main/java/ru/gb/JavaCore.java
@@ -0,0 +1,10 @@
+package ru.gb;
+
+public class JavaCore implements Course {
+    String name = "Java Core";
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/ru/gb/PythonCourse.java
@@ -0,0 +1,10 @@
+package ru.gb;
+
+public class PythonCourse implements Course {
+    String name = "Python Course";
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
